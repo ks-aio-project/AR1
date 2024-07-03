5797877156de53c347228b1cbde3e0f062da7cf6
ks-aio-project <allioneks@gmail.com>
2024-07-03 12:58:32 +0900

    12:58 저장

--- AR Project/Assets/Scripts/ObjectInitPosition.cs
+++ AR Project/Assets/Scripts/ObjectInitPosition.cs
@@ -6,11 +6,55 @@ using UnityEngine.XR.ARFoundation;
 
 public class ObjectInitPosition : MonoBehaviour
 {
-    public GameObject room;
+    public GameObject room1, room2, room3;
 
-    private void Start() {
-        var instance = Instantiate(room);
-        instance.transform.position = Camera.main.transform.position;
-        instance.transform.parent = null;
+    public TextMeshProUGUI tmp;
+    DefaultObserverEventHandler[] observerEventHandlers;
+
+    int count = 0;
+
+    private void Start()
+    {
+        // Scene에서 모든 DefaultObserverEventHandler를 찾아서 가져옴
+        observerEventHandlers = FindObjectsOfType<DefaultObserverEventHandler>();
+
+        // 각 DefaultObserverEventHandler에 대해 구독 설정
+        foreach (var observer in observerEventHandlers)
+        {
+            observer.OnTargetFound.AddListener(() => OnTargetFoundHandler(observer.transform));
+        }
+    }
+
+    // OnTargetFound 이벤트 발생 시 호출될 메서드
+    private void OnTargetFoundHandler(Transform observer)
+    {
+        Debug.Log("Target Found!");
+        
+        count++;
+
+        tmp.text = $"Target Found / Count : {count}";
+
+        switch(observer.transform.name)
+        {
+            case "ImageTarget1":
+            room1.SetActive(true);
+            room1.transform.position = Camera.main.transform.position;
+            room2.SetActive(false);
+            break;
+            case "ImageTarget2":
+            room1.SetActive(false);
+            room2.SetActive(true);
+            room2.transform.position = Camera.main.transform.position;
+            break;
+        }
+    }
+
+    private void OnDestroy()
+    {
+        // 각 DefaultObserverEventHandler에서 구독 해제
+        foreach (var observer in observerEventHandlers)
+        {
+            observer.OnTargetFound.RemoveListener(() => OnTargetFoundHandler(observer.transform));
+        }
     }
 }
